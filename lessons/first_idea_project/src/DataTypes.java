7733994a848917203e7593cebea55495ff8eea2e
raisatya <satyanlpid1@gmail.com>
2024-06-27 17:44:33 +0530

    First commit

---
+++ lessons/first_idea_project/src/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Primitive Types
+        int rollno = 1;
+        char charname = 'a';
+        float marks = 45.50f;
+        double largeDecimalNumbers = 437442424.2434;
+        long largeInteger = 3434740310347930434L;
+        boolean isTrue = true;
+
+        System.out.println("Integer value: " + rollno);
+        System.out.println("Character value: " + charname);
+        System.out.println("Floating point value: " + marks);
+        System.out.println("Double value: " + largeDecimalNumbers);
+        System.out.println("Large Integer: " + largeInteger);
+        System.out.println("Boolean value: " + isTrue);
+    }
+}
